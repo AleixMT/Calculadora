ff85e26c6c60f6b9a66a7916b13234c797fde740
Aleix Marine <aleix.marine@estudiants.urv.cat>
2017-11-14 07:35:50 +0100

    Commit inicial

---
+++ Calculadora/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    /* Funcio bool comprova_gramatica(String liniaatractar). Comprova tants parentesis dentrada com de sortida. cerca la coincidencia de tots els caracters dins del vector de caracters possibles*/
+    /* Fer verso iterativa per 10 o  */
+    printf("Hello world!\n");
+    return 0;
+}
